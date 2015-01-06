6bb25b482fd6f87d87693d4aebba58827321072c
bryan hunt <github@binarytemple.co.uk>
2015-01-06 12:36:24 +0000

    Initial commit

---
+++ src/main/java/ExampleMain.java
@@ -0,0 +1,40 @@
+import com.sun.tools.javac.util.List;
+import org.javers.core.Javers;
+import org.javers.core.JaversBuilder;
+import org.javers.core.diff.Change;
+import org.javers.core.diff.Diff;
+
+import java.util.ListIterator;
+
+public class ExampleMain {
+    public static void main(String[] args) {
+        Foo one = new Foo("head", List.from(new Foo[]{new Foo("onechild")}));
+        Foo two = new Foo("head", List.from(new Foo[]{new Foo("onechild"), new Foo("twochild1"), new Foo("twochild2")}));
+
+//        Javers javers = JaversBuilder.javers().registerValueObject(Foo.class).typeSafeValues().build();
+       Javers javers = JaversBuilder.javers().build();
+
+        Diff compare = javers.compare(one, two);
+        System.err.println(compare);
+
+        java.util.List<Change> changes = compare. getChanges();
+
+        for (Change next : changes) {
+            if (next.getCommitMetadata().isPresent()) {
+                System.err.println(
+                        next.getCommitMetadata().get()
+                );
+                System.err.println(
+                        next.getAffectedGlobalId()
+                );
+            }
+            System.err.println(
+                    next.getAffectedCdo()
+            );
+        }
+
+        System.err.print(compare.getChanges());
+        System.err.print(compare.changesSummary());
+
+    }
+}
---
+++ src/main/java/Foo.java
@@ -0,0 +1,32 @@
+import org.javers.core.metamodel.annotation.Id;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Foo {
+
+    public Foo(String name) {
+        this.name = name;
+    }
+
+    public Foo(String name, List<Foo> children) {
+        this.name = name;
+        this.children = children;
+    }
+
+    public Foo() {
+    }
+
+    @Id
+    public String name = "";
+    public List<Foo> children = new ArrayList<Foo>();
+
+
+    @Override
+    public String toString() {
+        return "Foo{" +
+                "name='" + name + '\'' +
+                ", children=" + children +
+                '}';
+    }
+}
